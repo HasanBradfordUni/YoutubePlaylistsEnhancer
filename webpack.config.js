499790c2e56e6668f2847cb4c4e3f44374048fa1
Hasan BradfordUni <hakhta26@bradford.ac.uk>
2024-03-16 11:20:14 +0000

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
-// webpack.config.js
-import { DefinePlugin } from 'webpack';
+import { ProvidePlugin } from 'webpack';
 
 export const plugins = [
-    new DefinePlugin({
-        'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
-    })
+    // ... other plugins ...
+    new ProvidePlugin({
+        process: 'process/browser', // Provide a path to process/browser
+    }),
 ];
+
